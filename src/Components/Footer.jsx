f16da67e5f219b50713f64dad1be4875928f4e9e
Nils <nils.mehlhorn.fr@gmail.com>
2023-04-12 13:16:13 +0200

    footer fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,11 @@
 function Footer() {
+
     return (
 
         <div className='footer'>
-            <p className="french">I am seeking an internship opportunity within an enterprise that specializes in JavaScript, Node.js, React, HTML, CSS, and Express. The internship ideally should start on October 2nd and last for a period of 2.5 months. Although the internship is unpaid, I am willing to invest my time and effort to gain valuable experience, network with professionals in the industry, and develop my skills in the field.</p>
+            <p className="french">Je suis à la recherche d'une opportunité de stage au sein d'une entreprise spécialisée en JavaScript, Node.js, React, HTML, CSS et Express. Le stage devrait idéalement commencer le 2 octobre et durer 2,5 mois. Bien que le stage soit non rémunéré, je suis prêt(e) à investir mon temps et mes efforts pour acquérir une expérience précieuse, établir des contacts avec des professionnels de l'industrie et développer mes compétences dans le domaine.</p>
             <p className="english"> I am currently seeking an internship opportunity in an enterprise that focuses on JavaScript, Node.js, React, HTML, CSS, and Express. I am available from October 2nd for a period of 2.5 months and willing to work without compensation. I understand that unpaid internships provide valuable experience, networking opportunities, and a chance to enhance my skills in the field.</p>
-
+            <p className="trans">Je peut aussi prendre une alternance a partir du 2 octobre</p>
         </div>
     )
 }
