561bf12576cdc9c451c2401d7f1d13e694ebb388
Nils <nils.mehlhorn.fr@gmail.com>
2023-04-13 18:22:17 +0200

    ortho correction

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ function Footer() {
     return (
 
         <div className='footer'>
-            <p className="french">Je suis à la recherche d'une opportunité de stage au sein d'une entreprise spécialisée en JavaScript, Node.js, React, HTML, CSS et Express. Le stage devrait idéalement commencer le 2 octobre et durer 2,5 mois. Bien que le stage soit non rémunéré, je suis prêt(e) à investir mon temps et mes efforts pour acquérir une expérience précieuse, établir des contacts avec des professionnels de l'industrie et développer mes compétences dans le domaine.</p>
-            <p className="trans">Je peut aussi prendre une alternance a partir du 2 octobre</p>
-            <p className="french"><a className="mailto" href="mailto:nils.mehlhorn.fr@.com">Envoyé moi un mail</a></p>
+            <p className="french">Je suis à la recherche d'une opportunité de stage au sein d'une entreprise spécialisée en JavaScript, Node.js, React, HTML, CSS et Express. Le stage devrait idéalement commencer le 2 octobre 2023 et durer 2,5 mois. Bien que le stage soit non rémunéré, je suis prêt à investir mon temps et mes efforts pour acquérir une expérience précieuse, établir des contacts avec des professionnels de l'industrie et développer mes compétences dans le domaine.</p>
+            <p className="trans">Je peux aussi prendre une alternance à partir du 2 octobre 2023</p>
+            <p className="french"><a className="mailto" href="mailto:nils.mehlhorn.fr@.com">Envoyez-moi un mail</a></p>
 
         </div >
     )
--- src/assets/data.js
+++ src/assets/data.js
@@ -2,37 +2,32 @@ const data = [
 
     {
         "job": "Intérimaire  JUBIL INTERIM  ",
-        "date": "Juillet 2021 ",
+        "date": "2020-2022",
         "duties": "Manutentionnaire / Inventoriste / Aide déménageur / Opérateur de production",
     },
     {
         "job": "Assistant Manager en Boutique Informatique  ES2COM    ",
-        "date": "Mai 2014  Mai 2015",
+        "date": "2014-2015",
         "duties": "Support clientèle (dépannage, diagnostiques, prise de contrôle à distance, installation de logiciels, conseil) / Vente en magasin et en ligne / Pédagogie / Saisie de données informatiques Français - Anglais / Inventaire sur logiciel",
     },
 
     {
         "job": "Téléconseillé - XEROX  ",
-        "date": "Juillet 2013",
+        "date": "2013",
         "duties": "Support clientèle  Pédagogie / Saisie de données informatiques Français - Anglais ",
     },
     {
         "job": "Auto-entrepreneur Rainbow-Trade  ",
-        "date": "Mars 2011 Avr. 2013",
-        "duties": "(Vendeur sur Ebay et Amazon) Mars 2011 – Avr. 2013Saisie de données / Informatique HTML / Recensement organique des listings / Analyse de données(trafic et ventes)",
+        "date": "2011-2013",
+        "duties": "(Vendeur sur Ebay et Amazon) Saisie de données / Informatique HTML / Recensement organique des listings / Analyse de données (trafic et ventes)",
     },
     {
         "job": "Superviseur, serveur, formateur – Pizza Express Plc.  ",
-        "date": "Angleterre Nov. 1999 – Avr. 2013",
-        "duties": " Formation du personnel(hygiène, sécurité, standards) / Encadrement d’équipe / Gestion clientèle(réservations, résolution de plaintes) / Gestion de stock / Service clients",
+        "date": "Angleterre 1999-2013",
+        "duties": " Formation du personnel (hygiène, sécurité, standards) / Encadrement d’équipe / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock / Service clients",
 
     },
-    {
-        "job": "Serveur, formateur – Pizza Express Plc., Angleterre Nov. ",
-        "date": "1999 – Avr. 2013",
-        "duties": " Formation du personnel(hygiène, sécurité, standards) / Encadrement d’équipe / Gestion clientèle(réservations, résolution de plaintes) / Gestion de stock / Service clients ",
-
-    },
+    
 
     {
         "job": " Serveur – Royal Caribbean Cruises, USA ",
@@ -43,86 +38,85 @@ const data = [
     },
     {
         "job": "Serveur – Le Point Hôtel Restaurant, Edinburgh, Ecosse  ",
-        "date": "Juin 1998 – Avril 1999",
-        "duties": "Service client / Encaissements / Gestion clientèle(réservations, résolution de plaintes) / Gestion de stock",
+        "date": "1998-1999",
+        "duties": "Service client / Encaissements / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock",
 
     },
     {
-        "job": "Stages et emplois saisonniers, – ",
-        "date": "1990 – 1998 ",
-        "duties": " domaine restauration, service clients, service, encaissement, netoyage"
+        "job": "Emplois saisonniers",
+        "date": "1990-1998 "
     },
 
 
 
-    { "formation": "Formation Blue Stack (PHP, Front end) de Mai à Juillet 2021" },
-    { "formation": "Formation Développeur Web et Web Mobile de fevrier à Decembre 2023" },
+    { "formation": "Formation Blue Stack (PHP, Front End) de Mai à Juillet 2021" },
+    { "formation": "Formation Développeur Web et Web Mobile de février à Décembre 2023" },
 
 
-    { "12": "Anglais courant 'lu et parlé" },
+    { "12": "Anglais courant lu et parlé" },
     {
         "id": "100",
         "project": "Secondlife object ads clicker"
     },
     {
         "id": "101",
-        "project": "vente de livre libre de droit"
+        "project": "vente de livres libres de droit"
     },
     {
         "id": "102",
-        "project": "Dropishipping Amazon ebay"
+        "project": "Dropishipping Amazon - eBay"
     },
     {
         "id": "103",
-        "project": "website traffic antidetection project"
+        "project": "Website traffic antidetection project"
     },
     {
         "id": "104",
-        "project": "project de creation de dropshipping cdiscount"
+        "project": "projet de création de dropshipping Cdiscount"
     },
     {
         "id": "105",
-        "nuage": "gestion des clients"
+        "nuage": "Gestion des clients"
     },
     {
         "id": "106",
-        "nuage": "gestion des stocks"
+        "nuage": "Gestion des stocks"
     },
     {
         "id": "107",
-        "nuage": "gestion des commandes"
+        "nuage": "Gestion des commandes"
     },
     {
         "id": "108",
-        "nuage": "gestion des livraisons"
+        "nuage": "Gestion des livraisons"
     },
     {
         "id": "109",
-        "nuage": "gestion des retours"
+        "nuage": "Gestion des retours"
     },
     {
         "id": "110",
-        "nuage": "gestion des factures"
+        "nuage": "Gestion des factures"
     },
     {
         "id": "111",
-        "nuage": "gestion des paiements"
+        "nuage": "Gestion des paiements"
     },
     {
         "id": "112",
-        "nuage": "optimisation de la surface de vente"
+        "nuage": "Optimisation de la surface de vente"
     },
     {
         "id": "113",
-        "nuage": "optimisation de la surface de stockage"
+        "nuage": "Optimisation de la surface de stockage"
     },
     {
         "id": "114",
-        "nuage": "optimisation de la surface de travail"
+        "nuage": "Optimisation de la surface de travail"
     },
     {
         "id": "115",
-        "nuage": "Visualisation des issues en temps reels et resolution imediate"
+        "nuage": "Visualisation des issues en temps réèl et résolution immédiate"
     },
     {
         "id": "116",
@@ -148,7 +142,7 @@ const data = [
     },
     {
         "id": "122",
-        "negociation": "Sens de la negociation"
+        "negociation": "Sens de la négociation"
     },
     {
         "id": "123",
@@ -177,7 +171,7 @@ const data = [
 
     {
         "id": "129",
-        "competences": ["Autonomie", "Gestion de projets", "Réactivité", "Controle de Qualité", "Curiosité", "Conviviabilité", "Gestion de crise", "Client statisfaction", "Negociation", "Rigueur", "Organisation", "Intégrité", "Javascript", "NodeJs", "React", "HTML", "CSS"]
+        "competences": ["Autonomie", "Gestion de projets", "Réactivité", "Contrôle Qualité", "Curiosité", "Conviviabilité", "Gestion de crise", "Satisfaction client", "Négociation", "Rigueur", "Organisation", "Intégrité", "Javascript", "NodeJs", "React", "HTML", "CSS"]
     },
     {
 
