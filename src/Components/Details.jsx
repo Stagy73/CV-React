ed52442aca15a592611b9dbfcfb340747b241f3d
Nils <nils.mehlhorn.fr@gmail.com>
2023-04-15 13:36:01 +0200

    latest gradient

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -7,7 +7,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function Details() {
-    return (
+    return (<div className="bigContainer">
+        
         <div className="info-container">
             <div className="name" >Nils Mehlhorn</div>
             <div className="address">1, Rue Amblard 26000 valence</div>
@@ -15,17 +16,19 @@ function Details() {
             <div className="permit">permit B</div>
             <div className="vehicle">véhiculé</div>
             <a href="tel:0619650978">
-                <div className="phone">06-19-65-09-78</div>
-            </a>
-
-            <div className="email">nils.mehlhorn.fr@gmail.com </div>
+            <div className="phone">06-19-65-09-78</div></a>
+                <div className="email">nils.mehlhorn.fr@gmail.com </div>
+                
+            
+            <div className ="animation">
             <div className="icone">
                 <img className="img4" src="/js-file.png" alt="React icon" width="90px" height="90px" />
                 <img className="img4" src="/atom.png" alt="JavaScript icon" width="90px" height="90px" />
                 <img className="img4" src="/html-5.png" alt="JavaScript icon" width="90px" height="90px" />
                 <img className="img4" src="/css.png" alt="JavaScript icon" width="90px" height="90px" />
                 <img className="img4" src="/node-js.png" alt="JavaScript icon" width="90px" height="90px" />
-
+</div>
+            </div>
             </div>
 
         </div >
