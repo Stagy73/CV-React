99125f607718b6e9f6aa533277ee2e2d6138b87e
Nils <nils.mehlhorn.fr@gmail.com>
2023-04-25 14:35:02 +0200

    emploi gologin &easync added + chiffre d'affaire dropshipping

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,192 +1,208 @@
- export const data = [
-
-    {
-        "job": "Intérimaire  JUBIL INTERIM  ",
-        "date": "2020-2022",
-        "duties": "Manutentionnaire / Inventoriste / Aide déménageur / Opérateur de production",
-    },
-    {
-        "job": "Assistant Manager en Boutique Informatique  ES2COM    ",
-        "date": "2014-2015",
-        "duties": "Support clientèle (dépannage, diagnostiques, prise de contrôle à distance, installation de logiciels, conseil) / Vente en magasin et en ligne / Pédagogie / Saisie de données informatiques Français - Anglais / Inventaire sur logiciel",
-    },
-
-    {
-        "job": "Téléconseillé - XEROX  ",
-        "date": "2013",
-        "duties": "Support clientèle  Pédagogie / Saisie de données informatiques Français - Anglais ",
-    },
-    {
-        "job": "Auto-entrepreneur Rainbow-Trade  ",
-        "date": "2011-2013",
-        "duties": "(Vendeur sur Ebay et Amazon) Saisie de données / Informatique HTML / Recensement organique des listings / Analyse de données (trafic et ventes)",
-    },
-    {
-        "job": "Superviseur, serveur, formateur – Pizza Express Plc., Angleterre",
-        "date": "1999-2013",
-        "duties": " Formation du personnel (hygiène, sécurité, standards) / Encadrement d’équipe / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock / Service clients",
-
-    },
-    
-
-    {
-        "job": " Serveur – Royal Caribbean Cruises, USA ",
-        "date": "Juin 1999 – Oct. 1999",
-        "duties": " Service client / Inventaires",
-
-
-    },
-    {
-        "job": "Serveur – Le Point Hôtel Restaurant, Edinburgh, Ecosse  ",
-        "date": "1998-1999",
-        "duties": "Service client / Encaissements / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock",
-
-    },
-    {
-        "job": "Emplois saisonniers",
-        "date": "1990-1998 "
-    },
-
-
-
-   
+export const data = [
   {
-    "formation": "Formation Blue Stack (PHP, Front End) de Mai à Juillet 2021 ",
-    "project": "Projet de développement web"
+    job: "Intérimaire  JUBIL INTERIM  ",
+    date: "2020-2022",
+    duties:
+      "Manutentionnaire / Inventoriste / Aide déménageur / Opérateur de production",
   },
   {
-    "formation": "Formation Développeur Web et Web Mobile de février à Décembre 2023 avec WildCodeSchool",
-    "project": "Projet de développement mobile"
+    job: "Assistant Manager en Boutique Informatique  ES2COM    ",
+    date: "2014-2015",
+    duties:
+      "Support clientèle (dépannage, diagnostiques, prise de contrôle à distance, installation de logiciels, conseil) / Vente en magasin et en ligne / Pédagogie / Saisie de données informatiques Français - Anglais / Inventaire sur logiciel",
   },
 
+  {
+    job: "Téléconseillé - XEROX  ",
+    date: "2013",
+    duties:
+      "Support clientèle  Pédagogie / Saisie de données informatiques Français - Anglais ",
+  },
+  {
+    job: "Auto-entrepreneur Rainbow-Trade  ",
+    date: "2011-2013",
+    duties:
+      " 13500 GBP/mois chiffre d'affaire(Vendeur sur Ebay et Amazon) Saisie de données / Informatique HTML / Recensement organique des listings / Analyse de données (trafic et ventes)",
+  },
+  {
+    job: "Auto-entrepreneur Gologin Antidetecttion & Easync Dropshipping chat support ",
+    date: "2011-2013",
+    duties:
+      "support informatique en tchat / Saisie de données / API support / Proxy Support / Auto-order support / Informatique HTML / Recensement organique des listings / Analyse de données (trafic et ventes)",
+  },
+  {
+    job: "Superviseur, serveur, formateur – Pizza Express Plc., Angleterre",
+    date: "1999-2013",
+    duties:
+      " Formation du personnel (hygiène, sécurité, standards) / Encadrement d’équipe / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock / Service clients",
+  },
 
+  {
+    job: " Serveur – Royal Caribbean Cruises, USA ",
+    date: "Juin 1999 – Oct. 1999",
+    duties: " Service client / Inventaires",
+  },
+  {
+    job: "Serveur – Le Point Hôtel Restaurant, Edinburgh, Ecosse  ",
+    date: "1998-1999",
+    duties:
+      "Service client / Encaissements / Gestion clientèle (réservations, résolution de plaintes) / Gestion de stock",
+  },
+  {
+    job: "Emplois saisonniers",
+    date: "1990-1998 ",
+  },
 
+  {
+    formation: "Formation Blue Stack (PHP, Front End) de Mai à Juillet 2021 ",
+    project: "Projet de développement web",
+  },
+  {
+    formation:
+      "Formation Développeur Web et Web Mobile de février à Décembre 2023 avec WildCodeSchool",
+    project: "Projet de développement mobile",
+  },
 
+  { 12: "Anglais courant lu et parlé" },
+  {
+    id: "100",
+    project: "Secondlife object ads clicker",
+  },
+  {
+    id: "101",
+    project: "vente de livres libres de droit",
+  },
+  {
+    id: "102",
+    project: "Dropishipping Amazon - eBay",
+  },
+  {
+    id: "103",
+    project: "Website traffic antidetection project",
+  },
+  {
+    id: "104",
+    project: "projet de création de dropshipping Cdiscount",
+  },
+  {
+    id: "105",
+    nuage: "Gestion des clients",
+  },
+  {
+    id: "106",
+    nuage: "Gestion des stocks",
+  },
+  {
+    id: "107",
+    nuage: "Gestion des commandes",
+  },
+  {
+    id: "108",
+    nuage: "Gestion des livraisons",
+  },
+  {
+    id: "109",
+    nuage: "Gestion des retours",
+  },
+  {
+    id: "110",
+    nuage: "Gestion des factures",
+  },
+  {
+    id: "111",
+    nuage: "Gestion des paiements",
+  },
+  {
+    id: "112",
+    nuage: "Optimisation de la surface de vente",
+  },
+  {
+    id: "113",
+    nuage: "Optimisation de la surface de stockage",
+  },
+  {
+    id: "114",
+    nuage: "Optimisation de la surface de travail",
+  },
+  {
+    id: "115",
+    nuage: "Visualisation des issues en temps réèl et résolution immédiate",
+  },
+  {
+    id: "116",
+    autonomie: "Autonomie",
+  },
 
-    { "12": "Anglais courant lu et parlé" },
-    {
-        "id": "100",
-        "project": "Secondlife object ads clicker"
-    },
-    {
-        "id": "101",
-        "project": "vente de livres libres de droit"
-    },
-    {
-        "id": "102",
-        "project": "Dropishipping Amazon - eBay"
-    },
-    {
-        "id": "103",
-        "project": "Website traffic antidetection project"
-    },
-    {
-        "id": "104",
-        "project": "projet de création de dropshipping Cdiscount"
-    },
-    {
-        "id": "105",
-        "nuage": "Gestion des clients"
-    },
-    {
-        "id": "106",
-        "nuage": "Gestion des stocks"
-    },
-    {
-        "id": "107",
-        "nuage": "Gestion des commandes"
-    },
-    {
-        "id": "108",
-        "nuage": "Gestion des livraisons"
-    },
-    {
-        "id": "109",
-        "nuage": "Gestion des retours"
-    },
-    {
-        "id": "110",
-        "nuage": "Gestion des factures"
-    },
-    {
-        "id": "111",
-        "nuage": "Gestion des paiements"
-    },
-    {
-        "id": "112",
-        "nuage": "Optimisation de la surface de vente"
-    },
-    {
-        "id": "113",
-        "nuage": "Optimisation de la surface de stockage"
-    },
-    {
-        "id": "114",
-        "nuage": "Optimisation de la surface de travail"
-    },
-    {
-        "id": "115",
-        "nuage": "Visualisation des issues en temps réèl et résolution immédiate"
-    },
-    {
-        "id": "116",
-        "autonomie": "Autonomie"
-    },
+  {
+    id: "118",
+    rigueur: "Rigueur",
+  },
+  {
+    id: "119",
+    autonomie: "Sens de l'initiative",
+  },
+  {
+    id: "120",
+    organisation: "Sens de l'organisation",
+  },
+  {
+    id: "121",
 
-    {
-        "id": "118",
-        "rigueur": "Rigueur"
-    },
-    {
-        "id": "119",
-        "autonomie": "Sens de l'initiative"
-    },
-    {
-        "id": "120",
-        "organisation": "Sens de l'organisation"
-    },
-    {
-        "id": "121",
+    satisfaction: "Sens de la communication",
+  },
+  {
+    id: "122",
+    negociation: "Sens de la négociation",
+  },
+  {
+    id: "123",
+    organisation: "Sens de la gestion",
+  },
+  {
+    id: "124",
+    "gestion de crise": "Sens de la gestion de crise",
+  },
+  {
+    id: "125",
+    "gestion de crise": "Sens de la gestion de conflit",
+  },
+  {
+    id: "126",
+    "gestion de projet": "Sens de la gestion de projet",
+  },
+  {
+    id: "127",
+    reactivite: "Sens de la gestion de temps",
+  },
+  {
+    id: "128",
+    reactivite: "Sens de la gestion de stress",
+  },
 
-        "satisfaction": "Sens de la communication"
-    },
-    {
-        "id": "122",
-        "negociation": "Sens de la négociation"
-    },
-    {
-        "id": "123",
-        "organisation": "Sens de la gestion"
-    },
-    {
-        "id": "124",
-        "gestion de crise": "Sens de la gestion de crise"
-    },
-    {
-        "id": "125",
-        "gestion de crise": "Sens de la gestion de conflit"
-    },
-    {
-        "id": "126",
-        "gestion de projet": "Sens de la gestion de projet"
-    },
-    {
-        "id": "127",
-        "reactivite": "Sens de la gestion de temps"
-    },
-    {
-        "id": "128",
-        "reactivite": "Sens de la gestion de stress"
-    },
-
-    {
-        "id": "129",
-        "competences": ["Autonomie", "Gestion de projets", "Réactivité", "Contrôle Qualité", "Curiosité", "Conviviabilité", "Gestion de crise", "Satisfaction client", "Négociation", "Rigueur", "Organisation", "Intégrité", "Javascript", "NodeJs", "React", "HTML", "CSS"]
-    },
-    {
-
-        "title": "WEB DEVELOPPEUR & WEB MOBILE"
-    },
-
-]
+  {
+    id: "129",
+    competences: [
+      "Autonomie",
+      "Gestion de projets",
+      "Réactivité",
+      "Contrôle Qualité",
+      "Curiosité",
+      "Conviviabilité",
+      "Gestion de crise",
+      "Satisfaction client",
+      "Négociation",
+      "Rigueur",
+      "Organisation",
+      "Intégrité",
+      "Javascript",
+      "NodeJs",
+      "React",
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    title: "WEB DEVELOPPEUR & WEB MOBILE",
+  },
+];
 export default data;
